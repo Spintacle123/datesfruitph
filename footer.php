aa742e8b66b60ca13cfefc6d0b48ca61d85f5c22
bryaw <127568366+bryaw@users.noreply.github.com>
2023-05-29 09:18:16 +0800

    fix: fix footer design, added icons (#6)

--- footer.php
+++ footer.php
@@ -4,36 +4,54 @@
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title></title>
-   <link rel="stylesheet" type="text/css" href="css/footer.css">
+   <!-- <link rel="stylesheet" type="text/css" href="css/footer.css"> -->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+<body>
     <div class="footer">
 		<div class="container" style="padding-top: 0px">
-			<div class="footer d-flex justify-content-between align-center">
-				<img src="../gogreen/assets/img/logo.png" style="width: 200px" alt="">
+			<div class="footer d-flex justify-content-between align-center foot">
 				<div class="flex">
-					<a href="https://www.facebook.com/datesfruitphilippines" style="color:white;">
-						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 256 256"><path fill="#1877F2" d="M256 128C256 57.308 198.692 0 128 0C57.308 0 0 57.307 0 128c0 63.888 46.808 116.843 108 126.445V165H75.5v-37H108V99.8c0-32.08 19.11-49.8 48.347-49.8C170.352 50 185 52.5 185 52.5V84h-16.14C152.958 84 148 93.867 148 103.99V128h35.5l-5.675 37H148v89.445c61.192-9.602 108-62.556 108-126.445"/><path fill="#FFF" d="m177.825 165l5.675-37H148v-24.01C148 93.866 152.959 84 168.86 84H185V52.5S170.352 50 156.347 50C127.11 50 108 67.72 108 99.8V128H75.5v37H108v89.445A128.959 128.959 0 0 0 128 256a128.9 128.9 0 0 0 20-1.555V165h29.825"/></svg>
-						Facebook
-					</a>
-					<a href="https://www.lazada.com.ph/shop/dates-fruit-ph" style="color:white;">
-						<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 48 48"><path fill="blue" stroke="white" stroke-linecap="round" stroke-linejoin="round" d="m34.382 6.28l-6.426 3.71c-2.183 1.26-5.732 1.254-7.928-.014L13.56 6.242L4.5 11.473l19.56 11.294L43.5 11.544L34.381 6.28Z"/><path fill="blue" stroke="white" stroke-linecap="round" stroke-linejoin="round" d="M43.5 11.544v19.27L24.966 41.517a1.81 1.81 0 0 1-1.81 0L4.5 30.745V11.473m19.561 11.294v18.987"/></svg>
-						Lazada
-					</a>
-					<a href="https://shopee.ph/SFA_33983595" style="color:white;">
-					<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24"><path fill="orange" d="M15.941 17.963c.23-1.879-.98-3.077-4.175-4.097c-1.548-.528-2.277-1.22-2.26-2.171c.065-1.056 1.048-1.825 2.352-1.85a5.29 5.29 0 0 1 2.883.89c.116.072.197.06.263-.04c.09-.144.315-.493.39-.62c.051-.08.061-.186-.068-.28c-.185-.137-.704-.415-.983-.532a6.47 6.47 0 0 0-2.511-.514c-1.91.008-3.413 1.215-3.54 2.826c-.081 1.163.495 2.107 1.73 2.827c.263.152 1.68.716 2.244.892c1.774.552 2.695 1.542 2.478 2.697c-.197 1.047-1.299 1.724-2.818 1.744c-1.203-.046-2.287-.537-3.127-1.19l-.141-.11c-.104-.08-.218-.075-.287.03c-.05.077-.376.547-.458.67c-.077.108-.035.168.045.234c.35.293.817.613 1.134.775a6.71 6.71 0 0 0 2.829.727a4.905 4.905 0 0 0 2.075-.354c1.095-.465 1.803-1.394 1.945-2.554zM12 1.401c-2.068 0-3.754 1.95-3.833 4.39h7.665C15.751 3.35 14.066 1.4 12 1.4zm7.851 22.598l-.08.001l-15.784-.002c-1.074-.04-1.863-.91-1.971-1.991l-.01-.195l-.707-15.526a.459.459 0 0 1 .45-.494h4.975C6.845 2.568 9.16 0 12 0c2.838 0 5.153 2.569 5.275 5.79h4.968a.459.459 0 0 1 .458.483l-.773 15.588l-.007.131c-.094 1.094-.979 1.977-2.07 2.006z"/></svg>
-						Shopee
-					</a>
+					<div style="margin-right: 400px">
+						<div class="-mt-6 ml-12 flex">
+							<a href="home.php" class="rounded-full h-24 w-24 overflow-hidden">
+								<img src="./assets/datesfruits.png" class="">
+							</a>
+						</div>
+						<h3>DATES FRUITS PH</h3>
+						<p class="ml-7">hello@datefruits.com.ph</p>
+					</div>
+					<div class="mt-3">
+						<p class="mt-5">Contact Us: </p>
+						<p class="con">(833-1931) / +639123456789</p>
+						<p class="mt-5">Follow Us: </p>
+						<div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+							<div class="bg-white w-12 h-12 rounded-full flex items-center justify-center">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M14 13.5h2.5l1-4H14v-2c0-1.03 0-2 2-2h1.5V2.14c-.326-.043-1.557-.14-2.857-.14C11.928 2 10 3.657 10 6.7v2.8H7v4h3V22h4v-8.5Z"/></svg>
+							</div>
+							<div class="bg-white w-12 h-12 rounded-full flex items-center justify-center">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23Z"/></svg>
+							</div>
+							<div class="bg-white w-12 h-12 rounded-full flex items-center justify-center">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7.8 2h8.4C19.4 2 22 4.6 22 7.8v8.4a5.8 5.8 0 0 1-5.8 5.8H7.8C4.6 22 2 19.4 2 16.2V7.8A5.8 5.8 0 0 1 7.8 2m-.2 2A3.6 3.6 0 0 0 4 7.6v8.8C4 18.39 5.61 20 7.6 20h8.8a3.6 3.6 0 0 0 3.6-3.6V7.6C20 5.61 18.39 4 16.4 4H7.6m9.65 1.5a1.25 1.25 0 0 1 1.25 1.25A1.25 1.25 0 0 1 17.25 8A1.25 1.25 0 0 1 16 6.75a1.25 1.25 0 0 1 1.25-1.25M12 7a5 5 0 0 1 5 5a5 5 0 0 1-5 5a5 5 0 0 1-5-5a5 5 0 0 1 5-5m0 2a3 3 0 0 0-3 3a3 3 0 0 0 3 3a3 3 0 0 0 3-3a3 3 0 0 0-3-3Z"/></svg>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
-			<hr>
+		</div>
+		<hr class="h-px bg-gray-200 border-0 dark:bg-gray-900 py-[2px]">
+		<div class="copyright">
 			<?php 
 				$date = date('Y');
-				echo '<p class="copyright">Copyright '.$date.' - Dates Fruits </p>';
+				echo '<p class="copyright">Copyright '.$date.' - Datesfruitsph All right reserved </p>';
 			?>
 		</div>
 	</div>
 	<style>
+		body{
+			background-color: #F7F2EC;
+		}
 		.container > div.footer {
 			display: flex;
 			flex-direction: column;
@@ -44,11 +62,35 @@
 			display: flex;
 			gap: 20px;
 		}
-
 		.flex < a {
 			 color: white !important;
 		}
+		.con{
+			margin-bottom: 2;
+			color: #DC7105
+		}
+		h3{
+			font-weight: bold;
+		}
+		p{
+			color: #707070;
+			font-size: 11px;
+		}
+		.copyright{
+			text-align: center;
+			margin-bottom: 15px
+		}
+		.logos > a > img {
+			width: 200px;
+			height: auto;
+		}
+		.logos {
+			display: flex;
+		}
 	</style>
+
+	<script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+</body>
 
 </html
  
