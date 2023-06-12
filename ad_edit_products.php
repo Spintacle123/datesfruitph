22e3b8b6110ad2ec6b3419dde4017516d81a1e06
Dani2601 <109335615+Dani2601@users.noreply.github.com>
2023-06-12 14:38:04 +0800

    feat: added multiple select images in EDIT GIFTING

--- ad_edit_products.php
+++ ad_edit_products.php
@@ -1,8 +1,8 @@
 <?php
-  include 'config.php';
-  include 'ad_action_product.php';
+include 'config.php';
+include 'ad_action_product.php';
 
-  error_reporting(0);
+error_reporting(0);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -36,9 +36,9 @@
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script> 
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/dt-1.10.18/datatables.min.js"></script>
-  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>  
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
   <!-- =======================================================
   * Template Name: NiceAdmin - v2.3.1
@@ -71,118 +71,118 @@
     </div><!-- End Page Title -->
 
     <section class="section">
-            
-        <div class="col-lg-6 mx-auto">
-            <div class="card">
-                <div>
-                    <div class="card-body">
-                        <a href="ad_addproducts.php" type="button" class="btn btn-info" style="float: right;">
-                        <i class="bx bxs-left-arrow-alt"></i></a>
-                        <br><br>
-                        <form action="ad_action_product.php" method="post"  enctype="multipart/form-data">
-                          <div class="row mb-3">
-                            
-                            <div class="row mb-3">
-                              
-                              <input type="hidden" name="id" value="<?= $id; ?>">
 
-                                <div class="col-sm-4">
-                                  <img src="<?= $image; ?>" width="130" height="135">
-                                </div>
-                                
-                                <!-- <label>Select Category: </label> -->
-                                <div class="col-sm-8">
-                                  <label>Select Category: </label>
-                                  <select name="class" value="<?= $class; ?>" class="form-select" aria-label="Default select example" required>
-                                    <!-- <select id="class" class="form-select" aria-label="Default select example" required> -->
-                                    <?php $sql_category = "SELECT * FROM categories"; ?>
-                                    <?php $qry_category = mysqli_query($conn, $sql_category); ?>
-                                    <?php while($get_category = mysqli_fetch_array($qry_category)) { ?>
-                                      <option value="<?php echo $get_category["cat_name"] ?>"><?php echo $get_category["cat_name"] ?></option>
-                                    <?php } ?>
-                                    </select>
+      <div class="col-lg-6 mx-auto">
+        <div class="card">
+          <div>
+            <div class="card-body">
+              <a href="ad_addproducts.php" type="button" class="btn btn-info" style="float: right;">
+                <i class="bx bxs-left-arrow-alt"></i></a>
+              <br><br>
+              <form action="ad_action_product.php" method="post" enctype="multipart/form-data">
+                <div class="row mb-3">
 
-                                    <label>Product Name: </label>
-                                    <input type="text" name="name" value="<?= $name; ?>" class="form-control" placeholder="Product Name">
-                                </div>
-                            </div>
+                  <div class="row mb-3">
 
-                            <div class="row mb-3">
-                              <div class="col-sm-6">
-                                <label>Product Code: </label>
-                                <input type="text" name="code" value="<?= $code; ?>" class="form-control" placeholder="Product Code">
-                              </div>
-                              
-                              <div class="col-sm-6">
-                                <label>Quantity: </label>
-                                <input type="text" name="prod_qntty" value="<?= $prod_qntty; ?>" class="form-control" placeholder="Quantity">
-                              </div>
-                            </div>
+                    <input type="hidden" name="id" value="<?= $id; ?>">
 
-                            <div class="row mb-3">
-                              <div class="col-sm-12">
-                                <label>Product Price: </label>
-                                <input type="text" name="price" value="<?= $price; ?>" class="form-control" placeholder="Price">
-                              </div>
-                            </div>
-
-                            <div class="row mb-3">
-                              <div class="col-sm-12">
-                                <label>Description: </label>
-                                <textarea type="text" name="description" class="form-control" value="<?= $description; ?>"></textarea>
-                              </div>
-                            </div>
-
-                            <?php
-                              $query="SELECT * FROM products_units WHERE product_id =?";
-                              $stmt=$conn->prepare($query);
-                              $stmt->bind_param("i", $id);
-                              $stmt->execute();
-                              $result=$stmt->get_result();
-                              $row=$result->fetch_all(MYSQLI_ASSOC);
-                            ?>
-                            <div class="row mb-3" x-data="{items: <?php echo htmlspecialchars(json_encode($row), ENT_QUOTES, 'UTF-8', true); ?>}" >
-                              <div class="col-sm-12">
-                                <label>Units: </label>  
-
-                                <template x-for="(item, index) in items" :key="index" >
-                                  <div class="input-group mb-2" >
-                                    <select x-model="item.unit" name="unit[]" class="form-select" >
-                                      <option>piece</option>
-                                      <option>gram</option>
-                                      <option>kilo</option>
-                                    </select>
-                                    <input x-model="item.unit_value" name="unit_value[]" type="text" value="test" class="form-control" placeholder="Unit value" />
-                                    <input x-model="item.unit_price" name="unit_price[]" type="text" value="test" class="form-control" placeholder="Price" />
-                                    <button class="btn btn-danger" @click.prevent="items.splice(index, 1)" >x</button>
-                                  </div>
-                                </template>
-
-                                <div @click="items.push({unit: '', unit_value: '', price: 0})" class="btn btn-primary" >Add product unit</div>
-
-                              </div>
-                            </div>
-
-                            <div class="row mb-3">
-                              <div class="col-sm-12">
-                                  <input class="form-control" type="file" name="images">
-                              </div>
-                            </div>
-
-                            <div class="row mb-3">
-                              <div class="col-sm-4">
-                                <input type="submit" name="update" class="btn btn-primary" value="Update Product">
-                              </div>
-                            </div>
-                            
-                        </form>
+                    <div class="col-sm-4">
+                      <img src="<?= $image; ?>" width="130" height="135">
                     </div>
-                </div>
+
+                    <!-- <label>Select Category: </label> -->
+                    <div class="col-sm-8">
+                      <label>Select Category: </label>
+                      <select name="class" value="<?= $class; ?>" class="form-select" aria-label="Default select example" required>
+                        <!-- <select id="class" class="form-select" aria-label="Default select example" required> -->
+                        <?php $sql_category = "SELECT * FROM categories"; ?>
+                        <?php $qry_category = mysqli_query($conn, $sql_category); ?>
+                        <?php while ($get_category = mysqli_fetch_array($qry_category)) { ?>
+                          <option value="<?php echo $get_category["cat_name"] ?>"><?php echo $get_category["cat_name"] ?></option>
+                        <?php } ?>
+                      </select>
+
+                      <label>Product Name: </label>
+                      <input type="text" name="name" value="<?= $name; ?>" class="form-control" placeholder="Product Name">
+                    </div>
+                  </div>
+
+                  <div class="row mb-3">
+                    <div class="col-sm-6">
+                      <label>Product Code: </label>
+                      <input type="text" name="code" value="<?= $code; ?>" class="form-control" placeholder="Product Code">
+                    </div>
+
+                    <div class="col-sm-6">
+                      <label>Quantity: </label>
+                      <input type="text" name="prod_qntty" value="<?= $prod_qntty; ?>" class="form-control" placeholder="Quantity">
+                    </div>
+                  </div>
+
+                  <div class="row mb-3">
+                    <div class="col-sm-12">
+                      <label>Product Price: </label>
+                      <input type="text" name="price" value="<?= $price; ?>" class="form-control" placeholder="Price">
+                    </div>
+                  </div>
+
+                  <div class="row mb-3">
+                    <div class="col-sm-12">
+                      <label>Description: </label>
+                      <textarea type="text" name="description" class="form-control" value="<?= $description; ?>"><?php echo htmlspecialchars($description); ?></textarea>
+                    </div>
+                  </div>
+
+                  <?php
+                  $query = "SELECT * FROM products_units WHERE product_id =?";
+                  $stmt = $conn->prepare($query);
+                  $stmt->bind_param("i", $id);
+                  $stmt->execute();
+                  $result = $stmt->get_result();
+                  $row = $result->fetch_all(MYSQLI_ASSOC);
+                  ?>
+                  <div class="row mb-3" x-data="{items: <?php echo htmlspecialchars(json_encode($row), ENT_QUOTES, 'UTF-8', true); ?>}">
+                    <div class="col-sm-12">
+                      <label>Units: </label>
+
+                      <template x-for="(item, index) in items" :key="index">
+                        <div class="input-group mb-2">
+                          <select x-model="item.unit" name="unit[]" class="form-select">
+                            <option>piece</option>
+                            <option>gram</option>
+                            <option>kilo</option>
+                          </select>
+                          <input x-model="item.unit_value" name="unit_value[]" type="text" value="test" class="form-control" placeholder="Unit value" />
+                          <input x-model="item.unit_price" name="unit_price[]" type="text" value="test" class="form-control" placeholder="Price" />
+                          <button class="btn btn-danger" @click.prevent="items.splice(index, 1)">x</button>
+                        </div>
+                      </template>
+
+                      <div @click="items.push({unit: '', unit_value: '', price: 0})" class="btn btn-primary">Add product unit</div>
+
+                    </div>
+                  </div>
+
+                  <div class="row mb-3">
+                    <div class="col-sm-12">
+                      <input class="form-control" type="file" name="images">
+                    </div>
+                  </div>
+
+                  <div class="row mb-3">
+                    <div class="col-sm-4">
+                      <input type="submit" name="update" class="btn btn-primary" value="Update Product">
+                    </div>
+                  </div>
+
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </section>
 
-    
+
 
   </main><!-- End #main -->
 
@@ -206,13 +206,13 @@
 
 
   <script type="text/javascript">
-  $(document).ready(function() {
-    $('#data-table').DataTable({
-      paging: true
+    $(document).ready(function() {
+      $('#data-table').DataTable({
+        paging: true
+      });
     });
-  });
   </script>
-  
+
 </body>
 
 </html>
